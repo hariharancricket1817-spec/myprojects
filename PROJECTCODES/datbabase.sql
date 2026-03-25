1daca8f71abcc660552125d2ecc17be7249ccfc5
HARIHARAN <hariharancricket1817@gmail.com>
2026-03-25 11:12:48 +0530

    additions

--- PROJECTCODES/datbabase.sql
+++ PROJECTCODES/datbabase.sql
@@ -5,15 +5,15 @@ USE smart_college;
 CREATE TABLE students (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
-    reg_no VARCHAR(50),
-    roll_no VARCHAR(50),
+    reg_no int(50),
+    roll_no int(50),
     email VARCHAR(100),
     class VARCHAR(50),
     degree VARCHAR(50),
     department VARCHAR(100),
     hod_name VARCHAR(100),
     advisor_name VARCHAR(100),
-    phone VARCHAR(15),
+    phone int(15),
     username VARCHAR(50) UNIQUE,
     password VARCHAR(255)
 );
@@ -24,7 +24,7 @@ CREATE TABLE staff (
     staff_id VARCHAR(50),
     department VARCHAR(100),
     subject VARCHAR(100),
-    phone VARCHAR(15),
+    phone int(15),
     username VARCHAR(50) UNIQUE,
     password VARCHAR(255)
 );
